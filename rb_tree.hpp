e0a608a9a29389aa9e14faf5c9cadfa587d5d6d7
nonoaa <nonoaa1@naver.com>
2022-12-09 17:08:30 +0900

    fixed tree max_size

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -429,7 +429,7 @@ namespace ft
 
 		size_type max_size() const
 		{
-			return (_alloc.max_size());
+			return (_node_alloc.max_size());
 		}
 
 		/* ============================================================ */
